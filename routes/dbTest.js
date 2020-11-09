99a8aad9171fc740098c312b2cea48422e07487c
ksh030506 <llmm030506@naver.com>
2020-11-09 17:34:24 +0800

    [ Add ]
    DB connection 및 Test

--- routes/dbTest.js
+++ routes/dbTest.js
@@ -6,9 +6,7 @@ var dbconfig = require('../config/dbconfig');
 var connection = mysql.createConnection(dbconfig);
 connection.query('USE ' + dbconfig.database);
 
-var bodyParser = require('body-parser');
-
-router.get('/test', function(req, res) {
+router.get('/', function(req, res) {
     connection.query('SELECT * from Aca', function(err, rows, fields){
         if(!err){
             console.log(rows, fields);
--- web.js
+++ web.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 var app = express();
 var GetHtml = require('./routes/GetHtml');
 var UserApi = require('./routes/UserApi');
+var DbTest = require('./routes/dbTest');
 
 
 app.set('views', __dirname + '/views');
@@ -13,8 +14,10 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.use('/test', DbTest);
 app.use('/',GetHtml);
-app.use('/user', UserApi); 
+app.use('/user', UserApi);
+
 
 var server = app.listen(8080, function(){
     console.log("Express server has started on port 8080")
