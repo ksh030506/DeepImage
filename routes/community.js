1653c36a9c64391a428f82d651bbe8b40d974d12
ksh030506 <llmm030506@naver.com>
2020-11-27 18:55:23 +0800

    [ Add ]
    기본적인 로직 짜기

--- routes/community.js
+++ routes/community.js
@@ -50,11 +50,10 @@ app.get('/글쓰기 페이지', function(req, res){
 });
 
 app.post('/createComm', function(req, res){
-    var data = req.body;
-    var User = req.session.userEmail;
-    var title = data.title;
-    var content = data.content;
-    var writer = data.writer;
+    let data = req.body;
+    let title = data.title;
+    let content = data.content;
+    let writer = req.session.userEmail;
 
     connection.query('', function(err, rows, fields){
         if(err){
@@ -70,15 +69,67 @@ app.delete('/delete/:id', function(req, res){
     let UserSession = req.session.userEmail;
     let commid = req.params.id;
 
-    connection.query('이메일 검색 세션에 맞는 ');
+    connection.query('먼저 UsreSession이 DB에 들어가 있는 User와 일치한지 검사', function(err, rows, fields){
+        if(err){
+            console.log(err);
+        }
+        else {
+            connection.query('해당 게시물 삭제', function(err, rows, fields){
+                if(err){
+                    console.log(err);
+                }
+                else {
+                    console.log("성공");
+                }
+            });
+        }
+    });
 });
 
-app.patch('커뮤니티 글 수정', function(req, res){
+app.patch('커뮤니티 글 수정/:id', function(req, res){
+    let data = req.body;
+    let UserSession = req.session.userEmail;
+    let title = data.title;
+    let content = data.content;
+    let commid = req.params.id;
 
+    connection.query('먼저 UserSession이 DB에 들어가 있는 User와 일치한지 검사', function(err, rows, fields){
+        if(err){
+            console.log(err);
+        }
+        else {
+            connection.query('해당 게시물 수정', function(err, rows, fields){
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log("성공");
+                }
+            });
+        }
+    });
 });
 
 app.get('커뮤니티 글 보기', function(req, res){
+    connection.query('게시물 전체 보기 userid DESC와 나중에 페이징 처리', function(err, rows, fields){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("성공");
+        }
+    });
+});
 
+app.get('해당 글 보기/:id', function(req, res){
+    let commid = req.params.id;
+    // 만약 사용자가 맞지 않다면 수정할 수 없는 텍스트를 보여주고 사용자가 맞으면 수정 가능한 텍스트를 보여준다.
+    let UserSession = req.session.userEmail;
+    connection.query('해당 글 보기', function(err, rows, fields){
+        if(err){
+            console.log(err);
+        } else {
+            console.log("성공");
+        }
+    });
 });
 
 module.exports = app;
